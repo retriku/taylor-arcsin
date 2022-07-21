a216c03b0737e2c6f6cf4945a87f59a32a5d5305
Renārs Kudiņš <renars.kudins@gmail.com>
2022-07-21 09:51:42 +0300

    optimize

--- src/asin.rs
+++ src/asin.rs
@@ -1,8 +1,6 @@
 fn nn_fact_div_n_fact_squared(n: i32) -> f64 {
     if n == 0 {
         0.0
-    } else if n == 1 {
-        2.0
     } else {
         2.0 * (1..n).fold(1.0, |acc, i| acc * (((n + i) as f64) / i as f64))
     }
@@ -13,17 +11,18 @@ pub fn arcsine_as_sum(x: f64, epsilon: f64) -> f64 {
     let mut n: i32 = 1;
     let mut arcsin: f64 = x;
     let mut n4: f64 = 4.0;
-    let standard = x.asin();
     let mut xn: f64 = x;
 
-    while delta.abs() > epsilon {
+    while delta.abs() >= epsilon / 10.0 {
         let d: f64 = nn_fact_div_n_fact_squared(n);
         let c: f64 = 2.0 * n as f64 + 1.0;
         let x2np1: f64 = x * xn * xn;
 
-        arcsin += (d * x2np1) / (n4 * c);
+        let arcsin1 = arcsin + (d * x2np1) / (n4 * c);
 
-        delta = arcsin - standard;
+        delta = arcsin1 - arcsin;
+
+        arcsin = arcsin1.clone();
 
         n += 1;
         n4 *= 4.0;
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::time::Instant;
 
 mod asin;
 
+const EPSILON: f64 = 1e-10;
 const CHECK_TRIES: u32 = 10_000;
 const LOAD_TRIES: u64 = 10_000_000;
 
@@ -13,42 +14,41 @@ fn random_x(rng: &mut ThreadRng) -> f64 {
     rng.gen_range(0.0..1.0) * 1.8 - 0.9
 }
 
-fn check_correctness(epsilon: f64, rng: &mut ThreadRng) -> bool {
+fn check_correctness(rng: &mut ThreadRng) -> bool {
     (0..CHECK_TRIES)
         .map(|_| {
             let x = random_x(rng);
             let a = x.asin();
-            let b = asin::arcsine_as_sum(x, epsilon);
+            let b = asin::arcsine_as_sum(x, EPSILON);
 
             (a - b).abs()
         })
-        .all(|x: f64| x < epsilon)
+        .all(|x: f64| x < EPSILON)
 }
 
-fn check_speed(epsilon: f64, rng: &mut ThreadRng) {
+fn check_speed(rng: &mut ThreadRng) {
     println!("Generating test set...");
 
     println!("Testing...");
 
     let t_start = Instant::now();
     (0..LOAD_TRIES).map(|_| random_x(rng)).for_each(|x: f64| {
-        asin::arcsine_as_sum(x, epsilon);
+        asin::arcsine_as_sum(x, EPSILON);
         return ();
     });
     let t_end = Instant::now();
     let duration = t_end.duration_since(t_start);
 
-    let cps = LOAD_TRIES as f64/ duration.as_secs() as f64;
+    let cps = LOAD_TRIES as f64 / duration.as_secs() as f64;
     println!("Calls per second: {}", cps);
     // println!("Processing of load took: {:?}", duration);
 }
 
 fn main() {
-    let epsilon: f64 = 1e-10;
     let mut rng: ThreadRng = thread_rng();
 
-    let ok = check_correctness(epsilon, &mut rng);
+    let ok = check_correctness(&mut rng);
     if ok {
-        check_speed(epsilon, &mut rng);
+        check_speed(&mut rng);
     }
 }
